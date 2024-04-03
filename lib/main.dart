724c76aed73a6c6eaf945067be136c8b34b880a1
RAKIB <rakibulislam10.net@gmail.com>
2024-04-03 17:34:57 +0600

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:taskmanager/App.dart';
 
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+void main() {
   runApp(TaskManager());
 }
